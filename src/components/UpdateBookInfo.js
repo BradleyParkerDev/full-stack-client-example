a2a92cbd56f7060deb662f9faed421e3490df4b2
BradleyParkerDev <bradleyscottparker@outlook.com>
2023-03-15 10:02:37 -0500

    initial commit

--- src/components/UpdateBookInfo.js
+++ src/components/UpdateBookInfo.js
@@ -1,171 +1,148 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import '../App.css';
 
 const REACT_BACKEND = process.env.REACT_APP_ENDPOINT;
 
-class UpdateBookInfo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: '',
-      isbn: '',
-      author: '',
-      description: '',
-      published_date: '',
-      publisher: ''
-    };
-  }
+function UpdateBookInfo(props){
+  const [formData, setFormData] = useState({})
 
-  componentDidMount() {
-    // console.log("Print id: " + this.props.match.params.id);
+  const onChange = e => {
+    const {name, value} = e.target;
+
+    //update the form data on change 
+    setFormData({
+      ...formData, [name]:value
+    })
+  }
+  useEffect(() => {
     axios
-      .get(REACT_BACKEND+'/'+this.props.match.params.id)
+      .get(REACT_BACKEND+'/'+props.match.params.id)
       .then(res => {
         // this.setState({...this.state, book: res.data})
-        this.setState({
-          title: res.data.title,
-          isbn: res.data.isbn,
-          author: res.data.author,
-          description: res.data.description,
-          published_date: res.data.published_date,
-          publisher: res.data.publisher
-        })
+        console.log(res)
+        setFormData(res.data)
       })
       .catch(err => {
         console.log("Error from UpdateBookInfo");
       })
-  };
+    
+  }, [])
 
-  onChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
-
-  onSubmit = e => {
+  const onSubmit = e => {
     e.preventDefault();
 
-    const data = {
-      title: this.state.title,
-      isbn: this.state.isbn,
-      author: this.state.author,
-      description: this.state.description,
-      published_date: this.state.published_date,
-      publisher: this.state.publisher
-    };
-
-    axios
-      .put(REACT_BACKEND+'/'+this.props.match.params.id, data)
+    axios 
+      .put(REACT_BACKEND+'/'+props.match.params.id, formData)
       .then(res => {
-        this.props.history.push('/show-book/'+this.props.match.params.id);
+        props.history.push('/show-book/'+props.match.params.id);
       })
       .catch(err => {
         console.log("Error in UpdateBookInfo!");
       })
   };
 
-
-  render() {
-    return (
-      <div className="UpdateBookInfo">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-8 m-auto">
-              <br />
-              <Link to="/" className="btn btn-outline-warning float-left">
-                  Show BooK List
-              </Link>
-            </div>
-            <div className="col-md-8 m-auto">
-              <h1 className="display-4 text-center">Edit Book</h1>
-              <p className="lead text-center">
-                  Update Book's Info
-              </p>
-            </div>
-          </div>
-
+  return (
+    <div className="UpdateBookInfo">
+      <div className="container">
+        <div className="row">
           <div className="col-md-8 m-auto">
-          <form noValidate onSubmit={this.onSubmit}>
-            <div className='form-group'>
-              <label htmlFor="title">Title</label>
-              <input
-                type='text'
-                placeholder='Title of the Book'
-                name='title'
-                className='form-control'
-                value={this.state.title}
-                onChange={this.onChange}
-              />
-            </div>
             <br />
+            <Link to="/" className="btn btn-outline-warning float-left">
+                Show BooK List
+            </Link>
+          </div>
+          <div className="col-md-8 m-auto">
+            <h1 className="display-4 text-center">Edit Book</h1>
+            <p className="lead text-center">
+                Update Book's Info
+            </p>
+          </div>
+        </div>
 
-            <div className='form-group'>
-            <label htmlFor="isbn">ISBN</label>
-              <input
-                type='text'
-                placeholder='ISBN'
-                name='isbn'
-                className='form-control'
-                value={this.state.isbn}
-                onChange={this.onChange}
-              />
-            </div>
+        <div className="col-md-8 m-auto">
+        <form noValidate onSubmit={onSubmit}>
+          <div className='form-group'>
+            <label htmlFor="title">Title</label>
+            <input
+              type='text'
+              placeholder='Title of the Book'
+              name='title'
+              className='form-control'
+              value={formData.title}
+              onChange={onChange}
+            />
+          </div>
+          <br />
 
-            <div className='form-group'>
-            <label htmlFor="author">Author</label>
-              <input
-                type='text'
-                placeholder='Author'
-                name='author'
-                className='form-control'
-                value={this.state.author}
-                onChange={this.onChange}
-              />
-            </div>
-
-            <div className='form-group'>
-            <label htmlFor="description">Description</label>
-              <input
-                type='text'
-                placeholder='Describe this book'
-                name='description'
-                className='form-control'
-                value={this.state.description}
-                onChange={this.onChange}
-              />
-            </div>
-
-            <div className='form-group'>
-            <label htmlFor="published_date">Published Date</label>
-              <input
-                type='date'
-                placeholder='published_date'
-                name='published_date'
-                className='form-control'
-                value={this.state.published_date}
-                onChange={this.onChange}
-              />
-            </div>
-            <div className='form-group'>
-            <label htmlFor="publisher">Publisher</label>
-              <input
-                type='text'
-                placeholder='Publisher of this Book'
-                name='publisher'
-                className='form-control'
-                value={this.state.publisher}
-                onChange={this.onChange}
-              />
-            </div>
-
-            <button type="submit" className="btn btn-outline-info btn-lg btn-block">Update Book</button>
-            </form>
+          <div className='form-group'>
+          <label htmlFor="isbn">ISBN</label>
+            <input
+              type='text'
+              placeholder='ISBN'
+              name='isbn'
+              className='form-control'
+              value={formData.isbn}
+              onChange={onChange}
+            />
           </div>
 
+          <div className='form-group'>
+          <label htmlFor="author">Author</label>
+            <input
+              type='text'
+              placeholder='Author'
+              name='author'
+              className='form-control'
+              value={formData.author}
+              onChange={onChange}
+            />
+          </div>
+
+          <div className='form-group'>
+          <label htmlFor="description">Description</label>
+            <input
+              type='text'
+              placeholder='Describe this book'
+              name='description'
+              className='form-control'
+              value={formData.description}
+              onChange={onChange}
+            />
+          </div>
+
+          <div className='form-group'>
+          <label htmlFor="published_date">Published Date</label>
+            <input
+              type='date'
+              placeholder='published_date'
+              name='published_date'
+              className='form-control'
+              value={formData.published_date}
+              onChange={onChange}
+            />
+          </div>
+          <div className='form-group'>
+          <label htmlFor="publisher">Publisher</label>
+            <input
+              type='text'
+              placeholder='Publisher of this Book'
+              name='publisher'
+              className='form-control'
+              value={formData.publisher}
+              onChange={onChange}
+            />
+          </div>
+
+          <button type="submit" className="btn btn-outline-info btn-lg btn-block">Update Book</button>
+          </form>
         </div>
+
       </div>
-    );
-  }
-}
+    </div>
+  );
+}  
+
 
 export default UpdateBookInfo;
